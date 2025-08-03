24960c23c07fd2b453153a81740f5e6692d4915d
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-03 15:14:51 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,24 @@
+for i in range(1,11):
+    print(i)
+
+
+
+
+#break statement
+    for i in range(1,20):
+        if i ==10:
+            break
+        else:
+            print(i)
+
+
+
+
+
+
+            #1-50
+            for i in range(1,51):
+                if i==45:
+                    break 
+                else:
+                    print(i)
